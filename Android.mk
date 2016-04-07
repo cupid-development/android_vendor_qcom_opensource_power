e6031002edf00214dd17f460a183562e460f89f3
Christopher R. Palmer <crpalmer@gmail.com>
2016-04-07 06:13:45 -0400

    power: Allow devices to write their own platform specific bits
    Using this hook, any device should be able to reuse the majority of the
    powerhal and fork only power-<platform>.c if they feel that they need
    the ability to provide more specific tuning of the hints/etc. for
    their device.
    
    Change-Id: I013a3ec3ddccbe6f74f3dacf456cc6e8b3ab3430

--- Android.mk
+++ Android.mk
@@ -19,6 +19,10 @@ endif
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c Power.cpp main.cpp
 
+ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
+    LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
+else
+
 # Include target-specific files.
 ifeq ($(call is-board-platform-in-list, msm8974), true)
 LOCAL_SRC_FILES += power-8974.c
@@ -85,6 +89,8 @@ endif
 ifeq ($(call is-board-platform-in-list,msmnile), true)
 LOCAL_SRC_FILES += power-msmnile.c
 endif
+
+endif  #  End of board specific list
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
     LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
